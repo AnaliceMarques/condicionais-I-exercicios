f10e334616d35bc9bcd0f2bb0154d9ee1631e8a5
AnaliceMarques <analicemarquessl@gmail.com>
2022-11-30 12:01:38 -0300

    Resolução do Exercício 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,26 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = Number(prompt("Qual a sua idade?"));
+let terminouEnsinoMedio = prompt(
+  'Você já terminou o ensino médio? \nResponda "true" para sim e "false" para não.'
+);
+let cursandoFaculdade = prompt(
+  `Você não está cursando alguma faculdade? \nResponda "true" para confirmar e "false" para negar.`
+);
 
-if(){
+if (idade >= 18) {
+  console.log("A pessoa é maior de idade.");
+} else {
+  console.log("A pessoa é menor de idade.");
+}
 
-}else{
-	
+//Não está reconhecendo o true sem ser com as aspas.
+if (terminouEnsinoMedio == "true") {
+  console.log("A pessoa terminou o ensino médio.");
+} else {
+  console.log("A pessoa não terminou o ensino médio.");
+}
+
+if (cursandoFaculdade == "true") {
+  console.log("A pessoa não está cursando faculdade.");
+} else {
+  console.log("A pessoa está cursando faculdade.");
 }
